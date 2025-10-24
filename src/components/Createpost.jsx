3d094706a10616365542a3a221ba35cb5f5e6044
Abdelrahman-Afia <abdelrahmanafia20@gmail.com>
2025-10-24 10:14:16 +0300

    Post Creation Created

--- src/components/Createpost.jsx
+++ src/components/Createpost.jsx
@@ -1,20 +1,38 @@
 import React from "react";
-import "./Story.css";
+import "./Createpost.css";
 import img1 from "../Assets/post-profile.png";
 import img2 from "../Assets/live-icon.png";
 import img3 from "../Assets/photo-icon.png";
 import img4 from "../Assets/smile-face.png";
 
-const Story = () => {
+const Createpost = () => {
   return (
-    <div className="Story-container">
-      <img src={img1} alt="Story-img" className="Story-img" />
-      <div className="Story-user-details">
-        <img src={img2} alt="Story-user" className="Story-user" />
-        <p className="text">Islam Ali</p>
+    <div className="Post-container">
+      <div className="Post-top">
+        <img src={img1} alt="Post-profile" className="Post-profile" />
+        <div className="Type">What’s on your mind, Jenny</div>
+      </div>
+
+      <div className="Line"></div>
+
+      <div className="Post-bottom">
+        <div className="Live">
+          <img src={img2} alt="Live-icon" className="Live-icon" />
+          <p className="text">Live video</p>
+        </div>
+
+        <div className="Photo">
+          <img src={img3} alt="Live-icon" className="Photo-icon" />
+          <p className="text">Photo/video</p>
+        </div>
+
+        <div className="Smile">
+          <img src={img4} alt="Live-icon" className="Smile-icon" />
+          <p className="text">Feeling/activity</p>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Story;
+export default Createpost;
